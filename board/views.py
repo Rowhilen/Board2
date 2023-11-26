f025ace210b19448b3d136b66e0d51c508b73903
rowhilen <nephiana707@gmail.com>
2023-11-26 19:16:32 +0900

    first upload

---
+++ board/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+from .models import Board
+
+def index(request):
+    board_list = Board.objects.order_by('-create_date')
+    context = {'board_list': board_list}
+    return render(request, 'board/board_list.html', context)
+
+from .models import Board 
+from django.views import generic 
+from django.urls import reverse_lazy 
+def detail(request, board_id):
+    board = Board.objects.get(id=board_id)
+    context = {'board': board}
+    return render(request, 'board/board_detail.html', context) 
+
+class create(generic.CreateView):
+    model = Board
+    fields = ['subject', 'content', 'create_date']
+    success_url = reverse_lazy('board:list')
+    template_name_suffix = '_create'
